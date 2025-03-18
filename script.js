b170d14ddb711459624c274545e0aa8b61f8ebb3
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-03-18 23:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+const inputbtn=document.getElementById("newTodoInput");
+const todobtn=document.getElementById("addTodoBtn");
+const display= document.getElementById("todoList")
+todobtn.addEventListener("click",function(){
+	let inputValue = inputbtn.value;
+	if (inputValue.trim() !== "") {
+    let newTodo = document.createElement("li");
+    newTodo.textContent = inputValue;
+    display.appendChild(newTodo);
+    inputbtn.value = "";
+  } else {
+    // If input field is empty, show an alert or do nothing
+    alert("Cannot add an empty task");
+  }
+})
